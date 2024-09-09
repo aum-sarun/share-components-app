f64ce40ce633d7f739b91e909ac5d5552e361f13
Sarun <sarun.ng19@gmail.com>
2024-09-09 21:45:20 +1000

    feat(calendar): big-calendar-lib POC

---
+++ src/app/components/Calendar.tsx
@@ -0,0 +1,51 @@
+import { Calendar, momentLocalizer, Views } from "react-big-calendar";
+import moment from "moment";
+import { useCallback, useState } from "react";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+// Setup the localizer by providing the moment (or globalize, or Luxon) Object
+// to the correct localizer.
+const localizer = momentLocalizer(moment); // or globalizeLocalizer
+
+export default function MyCalendar() {
+  // /slove Next 14 issue
+  console.log("render!!!!");
+  const [date, setDate]: any = useState(new Date());
+  const [view, setView]: any = useState(Views.MONTH);
+  const onView = useCallback((newView: any) => setView(newView), [setView]);
+  const onNavigate = useCallback((curr: any) => setDate(new Date(curr)), [setDate]);
+  //  \slove Next 14 issue
+
+  const [events, setEvents] = useState([
+    {
+      title: "Team Meeting",
+      start: new Date(),
+      end: new Date(2024, 8, 15, 24),
+      allDay: false,
+    },
+    {
+      title: "Lunch with Client",
+      start: new Date(2024, 9, 15, 12, 0),
+      end: new Date(2024, 9, 15, 13, 0),
+      allDay: false,
+    },
+  ]);
+
+  return (
+    <div className="myCustomHeight">
+      <Calendar
+        localizer={localizer}
+        events={events}
+        views={[Views.MONTH, Views.WEEK, Views.DAY]}
+        style={{ height: 500 }}
+        
+        // to slove Next 14 issue
+        view={view} // Include the view prop
+        date={date} // Include the date prop
+        onNavigate={onNavigate}
+        onView={onView}
+        // \to slove Next 14 issue
+      />
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,54 +1,15 @@
 'use client';
 
 
-import { Table, DataFieldsEnum } from "./components/Table";
+import dynamic from 'next/dynamic';
+import Calendar from './components/Calendar';
 
 export default function Home() {
-  const columns = [
-    { header: "Name", field: DataFieldsEnum.NAME },
-    { header: "Location", field: DataFieldsEnum.LOCATION },
-    {
-      header: "Status",
-      render: (row: any) => (
-        <span
-          className={
-            row.status === "Active" ? "text-green-600" : "text-red-600"
-          }
-        >
-          {row.status}
-        </span>
-      ),
-    },
-    {
-      header: "Actions",
-      render: (row: any) => (
-        <div className="flex gap-x-6">
-          <button
-            onClick={() => console.log(row)}
-            className="items-center justify-center rounded-lg border px-3"
-          >
-            Edit
-          </button>
-          <button
-            onClick={() => console.log(row)}
-            className="items-center justify-center rounded-lg border px-3"
-          >
-            Delete
-          </button>
-        </div>
-      ),
-    },
-  ];
-
-  const data = [
-    { name: "Alice", age: 25, location: "New York", status: "Active" },
-    { name: "Bob", age: 30, location: "Los Angeles", status: "Inactive" },
-    { name: "Charlie", age: 35, location: "Chicago", status: "Active" },
-  ];
+  
 
   return (
     <>
-      <Table columns={columns} data={data}></Table>
+      <Calendar />
     </>
   );
 }
